44d587f1e58b4e5ba7b034ac1fdd2e922099804a
druswayne <mazaxak2@gmail.com>
2025-03-27 19:18:14 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import logging
+import asyncio
+
+from loader import *
+
+import handlers.voice
+
+
+
+async def main():
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(main())
